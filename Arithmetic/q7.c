e99101bb0cfee9a95b773ced8f9cde8522fc8a72
Navaneeth423 <navaneeth02052019@gmail.com>
2024-06-08 13:35:25 +0000

    Folder name changed

---
+++ Arithmetic/q7.c
@@ -0,0 +1,30 @@
+
+/*
+Author Name: Navaneeth M
+Grade
+DOC: 23rd May 2024
+*/
+
+#include<stdio.h>
+
+int main() 
+{
+	int marks;
+	printf("Enter the marks: \n");
+	scanf("%d", &marks);
+
+	if(marks>=90) {
+		printf("Grade A\n");
+	} else if(marks>=80) {
+		printf("Grade B\n");
+	} else if(marks>=70) {
+		printf("Grade C\n");
+	} else if(marks>=60) {
+		printf("Grade D\n");
+	} else {
+		printf("Grade F\n");
+	}
+	
+	
+	return 0;	
+}
